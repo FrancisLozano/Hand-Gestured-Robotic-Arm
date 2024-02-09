c4fc965029f391e5c998d8cfe63a035c8e482697
Benson Manzano <bensonfmanzano@gmail.com>
2024-02-08 16:45:50 -0800

    Updated motor functions to put in output of 4 motors

--- ForwardMovementCode.ino
+++ ForwardMovementCode.ino
@@ -1,17 +1,19 @@
-const int left_wheel = 6;
-const int right_wheel = 5;
+const int left_wheel_back = 6;
+const int right_wheel_back = 5;
+const int left_wheel_front = 9;
+const int right_wheel_front = 10;
 
 void setup() {
   // put your setup code here, to run once:
 
-  pinMode(left_wheel, OUTPUT);
-  pinMode(right_wheel, OUTPUT);
+  pinMode(left_wheel_back, OUTPUT);
+  pinMode(right_wheel_back, OUTPUT);
+  pinMode(left_wheel_front, OUTPUT);
+  pinMode(right_wheel_front, OUTPUT)
 
   pinMode(4, OUTPUT);
   pinMode(7, OUTPUT);
   pinMode(8, OUTPUT);
-  pinMode(9, OUTPUT);
-  pinMode(10, OUTPUT);
   pinMode(11, OUTPUT);
 
   // initializing motor pins 
@@ -21,16 +23,16 @@ void setup() {
 
 }
 
-void forward(){
-  digitalWrite(5, LOW)
-  digitalWrite(7, LOW)
-  digitalWrite(9, LOW)
-  digitalWrite(11, LOW)
-  digitalWrite(4, LOW)
-  digitalWrite(6, LOW)
-  digitalWrite(8, LOW)
-  digitalWrite(10, LOW)
-}
+// void forward(){
+//   digitalWrite(5, LOW)
+//   digitalWrite(7, LOW)
+//   digitalWrite(9, LOW)
+//   digitalWrite(11, LOW)
+//   digitalWrite(4, LOW)
+//   digitalWrite(6, LOW)
+//   digitalWrite(8, LOW)
+//   digitalWrite(10, LOW)
+// }
 
 void forward(int speed){
   // makes the robot go forward based on the speed specified 
@@ -39,24 +41,27 @@ void forward(int speed){
 
 void left(int speed){
   // makes the robot go left based on the speed specified
-  set_motor_speed(speed, 0);
+  set_motor_speed(0, speed, 0, speed);
 }
 
 void right(int speed){
   // makes the robot go right based on the speed specified
-  set_motor_speed(0, speed);
+  set_motor_speed(speed, 0, speed, 0);
 }
 
 void stop(){
   // stops all movements of the robot
-  set_motor_speed(0, 0);
+  set_motor_speed(0, 0, 0, 0);
 }
 
-void set_motor_speed(int left_speed, int right_speed){
+void set_motor_speed(int lsb, int rsb, int lsf, int rsf){
   // analog write does only work on 3, 5, 6, 9, 10, 11
   // takes in a parameter of left and right speed
-  analog.write(left_wheel, left_speed);
-  analog.write(right_wheel, right_speed);
+  analog.write(left_wheel_back, lsb);
+  analog.write(right_wheel_back, rsb);
+  analog.write(left_wheel_front, lsf);
+  analog.write(right_wheel_front, rsf);
+
 }
 
 void loop() {
