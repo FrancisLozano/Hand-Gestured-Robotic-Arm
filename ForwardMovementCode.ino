c2488b593b14dbc1782ad7e839530d2d8a1debae
Dommenick Lacuata <dlacuata@uci.edu>
2024-02-20 21:41:55 -0800

    Added flex sensor logic and arm forward code

--- ForwardMovementCode.ino
+++ ForwardMovementCode.ino
@@ -3,11 +3,21 @@
 
 int GRIP_SERVO_PIN  = 2;
 int CLAW_ROTATION_SERVO_PIN = 3;
-int flexPin = A5;
+int flexPin1 = A5;
+int flexPin2 = A6;
+int flexPin3 = A7;
+int flexPin4 = A8;
+int flexPin5 = A9;
+int flexPin6 = A10;
 
+int armPin = 4;
+
+int steps = 200;
 
 Servo ClawGrip;
 Servo ClawRotator;
+Servo ArmRotator;
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(4, OUTPUT);
@@ -18,18 +28,27 @@ void setup() {
   pinMode(9, OUTPUT);
   pinMode(10, OUTPUT);
   pinMode(11, OUTPUT);
-  pinMode(flexPin, INPUT)
+  pinMode(flexPin1, INPUT);
+  pinMode(flexPin2, INPUT);
+  pinMode(flexPin3, INPUT);
+  pinMode(flexPin4, INPUT);
+  pinMode(flexPin5, INPUT);
+  pinMode(flexPin6, INPUT);
   ClawGrip.attach(GRIP_SERVO_PIN);
   ClawRotator.attach(CLAW_ROTATION_SERVO_PIN);
-
+  ArmRotator.attach(armPin);
+  
   //Initialize starting positions for servos
   ClawGrip.write(90);
   ClawRotator.write(90);
+  
 
 }
 void clawGripActivate(float sensorData){
   int angle = map(sensorData,0,1023,0,180); //Adjust flex sensor readings
   ClawGrip.write(angle);
+
+  
 }
 
 void clawGripClose(){
@@ -45,6 +64,16 @@ void rotateClaw(float sensorData){
   ClawRotator.write(angle);
 }
 
+void armForward(float sensorData){
+  angle = map(sensorData,0,1023,0,90);
+  armRotator.write(angle)
+}
+
+void armBackward(float sensorData){
+  angle = map(sensorData,0,1023,90,180)
+  armRotator.write(angle)
+}
+
 
 void forward(){
   digitalWrite(5, LOW);
@@ -57,7 +86,52 @@ void forward(){
   digitalWrite(10, LOW);
 }
 
+
+
 void loop() {
   // put your main code here, to run repeatedly:
+  int thumbSensor = analogRead(flexPin1);
+  int ringSensor = analog(flexPin6);
+  int wristSensorF1 = analogRead(flexPin2);
+  int wristSensorF2 = analogRead(flexPin3);
+  int wristSensorB1 = analogRead(flexPin4);
+  int wristSensorB2 = analogRead(flexPin5);
+  int wristSensorForward = (wristSensorF1 + wristSensorF2) / 2;
+  int wristSensorBackward = (wristSensorB1 + wristSensorB2) / 2;
+
+  if(thumbSensor < 20){
+    clawGripClose();
+  } else {
+    clawGripOpen();
+  }
+
+  if (ringSensor < 20){
+    rotateClaw(ringSensor);
+  } else {
+    stop();
+  }
+
+  if (wristSensorForward > 20) {
+    //arm forward logic
+    armForward(wristSensorForward);
+    if(thumbSensor < 20){
+      clawGripClose();
+    } else {
+      clawGripOpen();
+    }
+
+    if (ringSensor < 20){
+      rotateClaw(ringSensor);
+    }
+
+  } else if (wristSensorbackward > 20){
+    //arm backward logic
+    armBackward(wristSensorBackward);
+  } else{
+    stop();
+  }
+
+
+
 
 }
